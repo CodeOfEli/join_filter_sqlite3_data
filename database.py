e5a88e250dae021d5c9f63dc5c4af0d5b0a4c75c
mac <casa@macs-MacBook.local>
2015-06-23 20:15:59 -0500

    Added Drop table if exists feature

--- database.py
+++ database.py
@@ -4,23 +4,58 @@ import sys
 
 
 
+
+
+cities = (('New York City', 'NY'),
+		  ('Boston', 'MA'),
+    	  ('Chicago', 'IL'),
+    	  ('Miami', 'FL'),
+    	  ('Dallas', 'TX'),
+    	  ('Seattle', 'WA'),
+    	  ('Portland', 'OR'),
+    	  ('San Francisco', 'CA'),
+    	  ('Los Angeles', 'CA'))
+
+
+weather = (('New York City', 2013, 'July', 'January', 62), 
+			('Boston', 2013, 'July', 'January', 59),
+			('Chicago', 2013, 'July', 'January', 59),
+			('Miami', 2013, 'August', 'January', 84), 
+			('Dallas', 2013, 'July', 'January', 77), 
+			('Seattle', 2013, 'July', 'January', 61),
+			('Portland', 2013, 'July', 'December', 63),
+			('San Francisco', 2013, 'September', 'December', 64),
+			('Los Angeles', 2013, 'September', 'December', 75))
+
+
 user_input = raw_input("What is the name of the city you would like to search for? ")
 # print user_input #works
 
 #Connect to db
-con = lite.connect('getting_started.db')
+con = lite.connect('getting_started2.db')
 
 with con: 
 
   cur = con.cursor()    
+  cur.execute("DROP TABLE IF EXISTS cities")
+  cur.execute("DROP TABLE IF EXISTS weather")
+
+  cur.execute("CREATE TABLE cities (name text, state text)")
+  cur.execute("CREATE TABLE weather (city text, year integer, warm_month text, cold_month text, average_high integer)")
+
+  cur.executemany("INSERT INTO cities VALUES(?, ?)", cities)
+  cur.executemany("INSERT INTO weather VALUES(?, ?, ?, ?, ?)", weather)
+
+
   cur.execute("SELECT * FROM cities WHERE name='{}'".format(user_input))
 
 
 
-# SELECT name, state, year, warm_month, cold_month 
-# FROM cities 
-# INNER JOIN weather 
-#     ON name = city;
+# TODO: Add an Inner Join Search: 
+	# SELECT name, state, year, warm_month, cold_month 
+	# FROM cities 
+	# INNER JOIN weather 
+	#     ON name = city;
 
 
 
